a3bf672764dd2ff968fa85e3548894168aeb1acc
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-04-05 20:25:19 -0300

    [A3] Registrando o interceptor

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,7 +1,6 @@
 import { environment } from './../../environments/environment';
-import { TokenService } from './../autenticacao/token.service';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animais, Animal } from './animais';
 
@@ -12,21 +11,13 @@ const API = environment.apiURL;
 })
 export class AnimaisService {
 
-  constructor(private http: HttpClient, private tokenService: TokenService) { }
+  constructor(private http: HttpClient) { }
 
   listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
-    const token = this.tokenService.retornaToken();
-    const headers = new HttpHeaders().append('x-access-token', token);
-    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
-      headers,
-    })
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`)
   }
 
   buscarAnimalPorId(id: number): Observable<Animal> {
-    const token = this.tokenService.retornaToken();
-    const headers = new HttpHeaders().append('x-access-token', token);
-    return this.http.get<Animal>(`${API}/photos/${id}`, {
-      headers,
-    })
+    return this.http.get<Animal>(`${API}/photos/${id}`);
   }
 }
