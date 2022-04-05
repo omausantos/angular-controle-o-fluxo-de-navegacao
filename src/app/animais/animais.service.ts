44ecc6f18dfdb85131546f936b17f54d61538b19
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-04-05 20:38:40 -0300

    [A3] Métodos de curtir e excluir foto

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,23 +1,38 @@
 import { environment } from './../../environments/environment';
-import { Observable } from 'rxjs';
+import { catchError, mapTo, Observable, of, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimaisService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
-    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`)
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
   }
 
   buscarAnimalPorId(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluirAnimalPorId(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.http
+      .post(`${API}/photos/${id}`, {}, { observe: 'response' })
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        }),
+      );
   }
 }
